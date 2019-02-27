83b420e93fda624dac1dd2852a6c6cf9432cdb55
Rinat R <evolticdev@gmail.com>
2019-02-27 18:42:41 +0500

    Create share now block

---
+++ src/components/universal/share-now/content/index.js
@@ -0,0 +1,64 @@
+const content = [
+  {
+    href:
+      'https://www.addtoany.com/add_to/facebook?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/facebook.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/twitter?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/twitter.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/google_plus?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/gplus.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/telegram?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote"',
+    image: require('./images/telegram.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/wechat?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/wechat.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/skype?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/skype.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/kakao?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/kakao.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/line?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/line.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/sina_weibo?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/sina.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/email?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote"',
+    image: require('./images/email.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/vk?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/vk.svg')
+  },
+  {
+    href:
+      'https://www.addtoany.com/add_to/mail_ru?linkurl=https%3A%2F%2Fplatinum.fund/listing%2F&linkname=http%3A%2F%2Fplatinum.fund/listing%2F&linknote=',
+    image: require('./images/mail.svg')
+  }
+]
+
+export default content

92f8d3fde3f2ab7b0932dc9b8bbcc4f5514a92c2
Rinat R <evolticdev@gmail.com>
2019-03-06 16:15:57 +0500

    Create show more component

---
+++ src/components/universal/show-more/ShowMore/listeners/removeEventListeners.js
@@ -0,0 +1,5 @@
+function removeEventListeners() {
+  this.toggleButton.removeEventListener('click', this.toggle)
+}
+
+export default removeEventListeners
---
+++ src/components/universal/show-more/style.js
@@ -0,0 +1,16 @@
+const style = `
+	<style>
+    .container__button {
+      font-family: 'Poppins', inherit, sans-serif;
+      font-style: normal;
+      font-weight: 300;
+      line-height: normal;
+      font-size: 2.9em;
+      color: #FF6932;
+      border-bottom: 1px dashed #000;
+      cursor: pointer;
+    }
+  </style>
+`
+
+export default style

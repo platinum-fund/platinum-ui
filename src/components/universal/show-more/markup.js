49bd1bb760bef1efe6a27d10cd9bb817a5f37f33
Rinat R <evolticdev@gmail.com>
2019-03-06 18:09:44 +0500

    Edit show-more component styles

--- src/components/universal/show-more/markup.js
+++ src/components/universal/show-more/markup.js
@@ -1,13 +1,11 @@
 const markup = `
-  <div class="container">
-    <slot name="preview"></slot>
-    <slot name="rest"></slot>
-    <slot name="toggle">
-      <div class="container__button">
-        see more
-      </div>
-    </slot>
-  </div>
+  <slot name="preview"></slot>
+  <slot name="rest"></slot>
+  <slot name="toggle">
+    <div class="toggle-button">
+      see more
+    </div>
+  </slot>
 `
 
 export default markup
--- src/components/universal/show-more/style.js
+++ src/components/universal/show-more/style.js
@@ -1,14 +1,22 @@
 const style = `
-	<style>
-    .container__button {
+	<style>	
+    .toggle-button {
+      margin-top: 1em;
+      margin-right: 0.7em;
       font-family: 'Poppins', inherit, sans-serif;
       font-style: normal;
       font-weight: 300;
       line-height: normal;
       font-size: 2.9em;
-      color: #FF6932;
-      border-bottom: 1px dashed #000;
+      color: rgba(0,0,0,0.4);
+      border-bottom: 1px dashed rgba(0,0,0,0.4);
       cursor: pointer;
+      transition: color 0.2s, border-bottom-color 0.2s;
+    }
+    
+    .toggle-button:hover {
+      color: #FF6932;
+      border-bottom: 1px dashed rgb(0,0,0);
     }
   </style>
 `
